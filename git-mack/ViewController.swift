92d30cf045b6ad135f0da8ec03ff2a9870122fc9
hoonjoo94 <hoonjoo94@naver.com>
2022-11-01 01:19:14 +0900

    Initial Commit

---
+++ git-mack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git-mack
+//
+//  Created by Hoonjoo Park on 2022/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
