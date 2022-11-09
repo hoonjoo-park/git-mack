dadaadc51026cf4bffb4ccb8073cc0cbb4b1263a
hoonjoo94 <hoonjoo94@naver.com>
2022-11-09 22:06:19 +0900

    infiniteScroll 기능 구현

--- git-mack/Models/Follower.swift
+++ git-mack/Models/Follower.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct Follower: Codable, Hashable {
+    var uuid = UUID().uuidString
+
+    private enum CodingKeys : String, CodingKey { case login, avatarUrl }
+    
     var login: String
     var avatarUrl: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
 }
--- git-mack/Screens/FollowerListVC.swift
+++ git-mack/Screens/FollowerListVC.swift
@@ -12,7 +12,9 @@ class FollowerListVC: UIViewController {
     enum Section { case main }
     
     var username: String!
+    var page = 1
     var followers: [Follower] = []
+    var hasMoreData = true
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -21,7 +23,7 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureDefault()
         configureCollectionView()
-        fetchFollowers()
+        fetchFollowers(username: username, page: page)
         configureDataSource()
     }
     
@@ -38,16 +40,19 @@ class FollowerListVC: UIViewController {
     func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(view: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = UIColor(r: 15, g: 24, b: 44)
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func fetchFollowers() {
+    func fetchFollowers(username: String, page: Int) {
         NetworkManager.shared.fetchFollowers(for: username, page: 1) { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .success(let followers):
+                if followers.count < 50 { self.hasMoreData = false }
+                
                 self.followers.append(contentsOf: followers)
                 self.updateData()
                 
@@ -69,6 +74,22 @@ class FollowerListVC: UIViewController {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
-        DispatchQueue.main.async { self.dataSource.apply(snapshot) }
+        DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+    }
+}
+
+extension FollowerListVC: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let scrollHeight = scrollView.contentSize.height
+        let containerHeight = scrollView.frame.size.height
+        
+        if offsetY + containerHeight + 50 >= scrollHeight {
+            guard hasMoreData else { return }
+            
+            page += 1
+            fetchFollowers(username: username, page: page)
+        }
     }
 }
